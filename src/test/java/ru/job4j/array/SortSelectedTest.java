6255674ed08847f8321a8ccf352eb671d2c39182
Veles1986 <ilyos.rafa@gmail.com>
2024-01-09 23:59:05 +0500

    6.5.3. Сортировка выборкой. [#156320]

--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SortSelectedTest {
     @Test
-    public void whenMySort() {
+    public void whenMySortForBigArray() {
         int[] data = new int[] {95, 41, 54, 25, 14, 93, 74, 52, 42, 42, 56, 54, 84, 19, 9, -5};
         int[] result = SortSelected.mySort(data);
         int[] expected = new int[] {-5, 9, 14, 19, 25, 41, 42, 42, 52, 54, 54, 56, 74, 84, 93, 95};
@@ -13,10 +13,26 @@ public class SortSelectedTest {
     }
 
     @Test
-    public void whenSort() {
+    public void whenMySortForSmallArray() {
+        int[] data = new int[] {9, 5, 6};
+        int[] result = SortSelected.mySort(data);
+        int[] expected = new int[] {5, 6, 9};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSortForBigArray() {
         int[] data = new int[] {95, 41, 54, 25, 14, 93, 74, 52, 42, 42, 56, 54, 84, 19, 9, -5};
         int[] result = SortSelected.sort(data);
         int[] expected = new int[] {-5, 9, 14, 19, 25, 41, 42, 42, 52, 54, 54, 56, 74, 84, 93, 95};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSortForSmallArray() {
+        int[] data = new int[] {9, 5, 6};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {5, 6, 9};
         assertThat(result).containsExactly(expected);
     }
 }
