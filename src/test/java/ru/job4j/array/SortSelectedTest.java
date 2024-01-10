60c55961e97634283e5647fe0b0483145fc4c30f
Veles1986 <ilyos.rafa@gmail.com>
2024-01-10 15:16:39 +0500

    6.5.3. Сортировка выборкой. [#156320 #472007]

--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -5,22 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SortSelectedTest {
     @Test
-    public void whenMySortForBigArray() {
-        int[] data = new int[] {95, 41, 54, 25, 14, 93, 74, 52, 42, 42, 56, 54, 84, 19, 9, -5};
-        int[] result = SortSelected.mySort(data);
-        int[] expected = new int[] {-5, 9, 14, 19, 25, 41, 42, 42, 52, 54, 54, 56, 74, 84, 93, 95};
-        assertThat(result).containsExactly(expected);
-    }
-
-    @Test
-    public void whenMySortForSmallArray() {
-        int[] data = new int[] {9, 5, 6};
-        int[] result = SortSelected.mySort(data);
-        int[] expected = new int[] {5, 6, 9};
-        assertThat(result).containsExactly(expected);
-    }
-
-    @Test
     public void whenSortForBigArray() {
         int[] data = new int[] {95, 41, 54, 25, 14, 93, 74, 52, 42, 42, 56, 54, 84, 19, 9, -5};
         int[] result = SortSelected.sort(data);
